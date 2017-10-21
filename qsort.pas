b9d3af7d79dd00832db21b4b73ec3b3a620626bf
Michaelwmx <Michaelwmx@users.noreply.github.com>
2017-10-22 01:13:31 +0800

    qsort

---
+++ qsort.pas
@@ -0,0 +1,39 @@
+program ascend_qs;
+const maxn=100000;
+var n,i:longint;
+	a:array[1..maxn]of longint;
+
+procedure swap(var x,y:longint);
+var t:longint;
+begin	
+	t:=x;
+	x:=y;
+	y:=t;
+end;
+
+procedure qs(l,r:longint);//ascend
+var x,y,k:longint;
+begin
+	x:=l;y:=r;
+	k:=a[(l+r)div 2];
+	repeat
+		while a[x]<k do inc(x);
+		while a[y]>k do dec(y);
+		if x<=y then
+		begin
+			swap(a[x],a[y]);
+			inc(x);
+			dec(y);
+		end;
+	until x>y;
+	if l<y then qs(l,y);
+	if r>x then qs(x,r);
+end;
+
+begin
+	readln(n);
+	for i:=1 to n do read(a[i]);
+	qs(1,n);
+	for i:=1 to n do
+	write(a[i],' ');
+end.
